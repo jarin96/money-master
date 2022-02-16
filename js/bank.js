d59cf6441771ee9c4096bae3f70e840944546f4a
jarin96 <2017-1-60-027@std.ewubd.edu>
2022-02-17 00:57:59 +0600

    income part added

--- js/bank.js
+++ js/bank.js
@@ -1,1 +1,31 @@
-console.log('clicked');
+document.getElementById('calculate-button').addEventListener('click', function () {
+    const foodInput = document.getElementById('food-input');
+    const foodAmount = foodInput.value;
+    // console.log(incomeAmount);
+    // get current income
+    const totalExpence = document.getElementById('total-expence');
+    const totalExpenceText = totalExpence.innerText;
+    totalExpence.innerText = foodAmount;
+    // console.log(totalExpenceText);
+    // clear input field
+    foodInput.value = '';
+    // get current rent
+    const rentInput = document.getElementById('rent-input');
+    const rentAmount = rentInput.value;
+    const totalExp = document.getElementById('total-expence');
+    const totalExpText = totalExp.innerText;
+    totalExp.innerText = rentAmount;
+    // console.log(totalExpText);
+    // clear input field
+    rentInput.value = '';
+    // get current cloth
+    const clothInput = document.getElementById('cloth-input');
+    const clothAmount = clothInput.value;
+    const totalExpe = document.getElementById('total-expence');
+    const totalExpeText = totalExpe.innerText;
+    totalExp.innerText = clothAmount;
+    // console.log(totalExpeText);
+    const totalCalculation = parseFloat(totalExpeText) + parseFloat(totalExpText) + parseFloat(totalExpeText);
+    console.log(totalCalculation);
+
+})
