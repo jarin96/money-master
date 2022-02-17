b1f479ee7d77df6a1171e52cd2fc1dc5a033c89d
jarin96 <2017-1-60-027@std.ewubd.edu>
2022-02-17 11:51:43 +0600

    saving amount part

--- js/bank.js
+++ js/bank.js
@@ -32,4 +32,19 @@ document.getElementById('calculate-button').addEventListener('click', function (
     totalBalance.innerText = incomeAmount;
     totalCalculation = incomeAmount - totalCalculation;
     totalBalance.innerText = totalCalculation;
-})
+});
+const discountAmount = 20;
+document.getElementById('save-btn').addEventListener('click', function () {
+    const code = document.getElementById('save-amount').value;
+    if (code == discountAmount) {
+        const total = document.getElementById('save-price');
+        const savingAmount = (10000 * 20) / 100;
+        total.innerText = savingAmount;
+    }
+    else {
+        console.log('please give 20% amount');
+    }
+    const Balance = document.getElementById('remain-balance');
+    const remainingBalance = totalCalculation - savingAmount;
+    Balance.value = remainingBalance;
+});
